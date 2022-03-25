1df8fb1b60248e13c3874d454721e7ebd40c6a0d
Minji Kim <minji_0801@naver.com>
2022-03-25 21:59:22 +0900

    Xcode Project 생성

---
+++ FilmRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmRecord
+//
+//  Created by 김민지 on 2022/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
